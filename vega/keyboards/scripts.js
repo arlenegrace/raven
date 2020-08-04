796e8de7f2f4dcc6942bf230003a0500d6423b11
arlengrace <63443490+arlengrace@users.noreply.github.com>
2020-08-03 20:47:06 -0700

    feat: redesigned desktop layout

--- vega/keyboards/scripts.js
+++ vega/keyboards/scripts.js
@@ -15,3 +15,17 @@ function open_filter() {
         gallery.style.display = "none";
     }
 }
+
+function pick_filter(x) {
+    const filters = document.getElementsByClassName('filter');
+    const activeFilter = document.getElementById('active_filter');
+
+    if (this !== activeFilter) {
+        return;
+    }
+
+    for (let i = 0; i < filters.length; i++) {
+        filters[i].classList.remove("active_filter");
+    }
+    x.classList.add("active_filter");
+}
