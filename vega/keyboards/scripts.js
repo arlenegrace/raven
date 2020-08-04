7b04161b5b2469193d29e0997127d19f5e286aae
arlengrace <63443490+arlengrace@users.noreply.github.com>
2020-08-04 00:16:45 -0700

    feat: redesigned mobile layouts

--- vega/keyboards/scripts.js
+++ vega/keyboards/scripts.js
@@ -1,21 +1,3 @@
-function open_filter() {
-    const btn = document.getElementById('filter_btn');
-    const sidebar = document.getElementById('sidebar');
-    const gallery = document.getElementById('gallery');
-    const opened = btn.classList.contains("open");
-    btn.classList.toggle("open");
-
-    if (opened) {
-        //close
-        sidebar.style.display = "none";
-        gallery.style.display = "block";
-    } else {
-        //open
-        sidebar.style.display = "block";
-        gallery.style.display = "none";
-    }
-}
-
 function pick_filter(x) {
     const filters = document.getElementsByClassName('filter');
     const activeFilter = document.getElementById('active_filter');
